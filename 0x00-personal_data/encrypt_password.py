eaac02106121a098bcc7013518d94848e159c230
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-03-18 22:43:01 -0700

    hash

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hashing passwords """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt(12))
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ validate password """
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
