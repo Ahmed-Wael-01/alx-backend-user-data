8b90d38c65272e77e21d84db3930ef39701313d4
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-03-18 21:42:32 -0700

    zero

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 filter datum
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     logging messages
     """
