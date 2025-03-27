95cdb93981982fc2f7e1bd03638c3dfac576338d
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-03-27 03:14:35 -0700

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ AUTHENTICATION CLASS
 """
+from base64 import b64decode
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
@@ -18,3 +19,16 @@ class BasicAuth(Auth):
                 not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ decode base64 auth header
+        """
+        if base64_authorization_header is None or\
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
