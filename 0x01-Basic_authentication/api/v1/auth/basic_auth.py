c40ecefbe43adeb8a7b593ad1b411e63f2c27814
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-03-27 02:22:18 -0700

    new basicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" AUTHENTICATION CLASS
+"""
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ authentication class
+    """
+    pass
