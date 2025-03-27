ec350e97ac795fe3a0eee2dc572bcb9b47c5a641
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-03-27 03:27:16 -0700

    allowing columns in passwords

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,5 +42,5 @@ class BasicAuth(Auth):
                 type(decoded_base64_authorization_header) is not str or\
                 ':' not in decoded_base64_authorization_header:
             return None, None
-        creds = decoded_base64_authorization_header.split(':')
+        creds = decoded_base64_authorization_header.split(':', 1)
         return creds[0], creds[1]
