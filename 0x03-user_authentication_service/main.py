f4410fa418c1b1195b87a5be06d801a0f2268eb0
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-04-12 05:37:20 -0700

    integeration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
+""" test a full user experience case
 """
-Main file
-"""
-from auth import Auth
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
-
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+pass
