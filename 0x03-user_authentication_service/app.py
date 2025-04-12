0a8ab7180879b48a139a9b6bebea4a3593860896
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-04-12 04:17:48 -0700

    session endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ the flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,18 @@ def users():
                         "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'])
+def login():
+    """ login route
+    """
+    if not AUTH.valid_login(request.form['email'], request.form['password']):
+        abort(401)
+    session_id = AUTH.create_session(request.form['email'])
+    res = jsonify({"email": request.form['email'], "message": "logged in"})
+    res.set_cookie('session_id', session_id)
+    return res
 
 
 if __name__ == "__main__":
