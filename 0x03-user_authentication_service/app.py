f09bb1e6cad952076419b29bbef6e3ca33336b41
Ahmed-Wael-01 <xahmedwaheed@gmail.com>
2025-04-12 03:24:00 -0700

    users endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ the flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -12,6 +14,18 @@ def openning():
     """ beginning route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """ users end point
+    """
+    try:
+        AUTH.register_user(request.form['email'], request.form['password'])
+        return jsonify({"email": request.form['email'],
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
